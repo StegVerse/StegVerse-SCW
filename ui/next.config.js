6563f2f2c4e3cf687fcff810b3c9824d38657b79
StegVerse <rigelrandolph@gmail.com>
2025-09-14 22:56:20 -0500

    Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = { output: 'export', images: { unoptimized: true } };
+module.exports = nextConfig;/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   output: 'export'
